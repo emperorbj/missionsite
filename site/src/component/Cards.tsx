2cc90b7eb154570d73f7fc411459532fbc0630f8
emperorbj <cwaku96@gmail.com>
2024-05-06 14:32:46 -0700

    worked on each card

--- site/src/component/Cards.tsx
+++ site/src/component/Cards.tsx
@@ -2,23 +2,51 @@ import React from 'react';
 
 const Cards = () => {
     return (
-        <section>
-            <h1 className='text-2xl font-bold '> OUR CORE MISSION AND VISION</h1>
+        <section className='flex flex-col bg-blue-600'>
+            <h1 className='text-2xl font-bold text-center my-[20px]'> OUR CORE MISSION AND VISION</h1>
             {/* card container */}
-            <div>
+            <div className='bg-green-400 flex gap-10 px-[30px] pb-[10px] h-[500px]'>
                 {/* Each card design */}
-                <div>
+                <div className='flex flex-col bg-orange-300 h-[460px]'>
                     <div>
-                        <img src="" alt="" />
+                        <img className='bg-yellow-400 h-[260px]' src="" alt="card-image" />
+                    </div>
+                    <div>
+                        <h1>Training</h1>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                            Corrupti labore voluptatem voluptatum adipisci tenetur
+                            placeat veniam iusto ducimus repellat blanditiis eveniet, 
+                            mollitia error voluptatibus aspernatur delectus ea qui nihil 
+                            praesentium?</p>
                     </div>
                 </div>
 
-                <div>
-
+                <div className='bg-red-300 h-[460px]'>
+                    <div>
+                        <img className='bg-yellow-400 h-[260px]' src="" alt="card-image" />
+                    </div>
+                    <div>
+                        <h1>Training</h1>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                            Corrupti labore voluptatem voluptatum adipisci tenetur
+                            placeat veniam iusto ducimus repellat blanditiis eveniet, 
+                            mollitia error voluptatibus aspernatur delectus ea qui nihil 
+                            praesentium?</p>
+                    </div>
                 </div>
 
-                <div>
-
+                <div className='bg-purple-500 h-[460px]'>
+                    <div>
+                        <img className='bg-yellow-400 h-[260px]' src="" alt="card-image" />
+                    </div>
+                    <div>
+                        <h1>Training</h1>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                            Corrupti labore voluptatem voluptatum adipisci tenetur
+                            placeat veniam iusto ducimus repellat blanditiis eveniet, 
+                            mollitia error voluptatibus aspernatur delectus ea qui nihil 
+                            praesentium?</p>
+                    </div>
                 </div>
             </div>
         </section>
