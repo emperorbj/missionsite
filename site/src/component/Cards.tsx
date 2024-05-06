84fc0a662d71e6af94fcccf41e983fd712c6454c
emperorbj <cwaku96@gmail.com>
2024-05-06 15:32:19 -0700

    styled each card section in details

--- site/src/component/Cards.tsx
+++ site/src/component/Cards.tsx
@@ -1,19 +1,21 @@
-import React from 'react';
+import Training from '../assets/training.jpg';
+import Prayer from '../assets/prayer.jpg';
+import Outreach from '../assets/outreach.jpg';
 
 const Cards = () => {
     return (
-        <section className='flex flex-col bg-blue-600'>
+        <section className='flex flex-col'>
             <h1 className='text-2xl font-bold text-center my-[20px]'> OUR CORE MISSION AND VISION</h1>
             {/* card container */}
-            <div className='bg-green-400 flex gap-10 px-[30px] pb-[10px] h-[500px]'>
+            <div className='flex gap-10 px-[30px] pb-[10px] h-[500px]'>
                 {/* Each card design */}
-                <div className='flex flex-col bg-orange-300 h-[460px]'>
-                    <div>
-                        <img className='bg-yellow-400 h-[260px]' src="" alt="card-image" />
+                <div className='flex flex-col h-[460px] border-2 bg-slate-100 rounded-xl shadow-lg'>
+                    <div className='h-[260px]' >
+                        <img className='h-[100%] w-[100%] rounded-xl' style={{objectFit:'cover', objectPosition:'center'}}  src={Training} alt="card-image" />
                     </div>
                     <div>
-                        <h1>Training</h1>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                        <h1 className='text-center text-2xl font-medium'>Training</h1>
+                        <p className='px-[20px] text-center'>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
                             Corrupti labore voluptatem voluptatum adipisci tenetur
                             placeat veniam iusto ducimus repellat blanditiis eveniet, 
                             mollitia error voluptatibus aspernatur delectus ea qui nihil 
@@ -21,13 +23,13 @@ const Cards = () => {
                     </div>
                 </div>
 
-                <div className='bg-red-300 h-[460px]'>
-                    <div>
-                        <img className='bg-yellow-400 h-[260px]' src="" alt="card-image" />
+                <div className='flex flex-col h-[460px] border-2 bg-slate-100 rounded-xl shadow-lg'>
+                    <div className='h-[260px]'>
+                        <img className='h-[100%] w-[100%] rounded-xl ' style={{objectFit:'cover', objectPosition:'center'}}  src={Prayer} alt="card-image" />
                     </div>
                     <div>
-                        <h1>Training</h1>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                        <h1 className='text-center text-2xl font-medium'>Prayers</h1>
+                        <p className='px-[20px] text-center'>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
                             Corrupti labore voluptatem voluptatum adipisci tenetur
                             placeat veniam iusto ducimus repellat blanditiis eveniet, 
                             mollitia error voluptatibus aspernatur delectus ea qui nihil 
@@ -35,13 +37,13 @@ const Cards = () => {
                     </div>
                 </div>
 
-                <div className='bg-purple-500 h-[460px]'>
-                    <div>
-                        <img className='bg-yellow-400 h-[260px]' src="" alt="card-image" />
+                <div className='flex flex-col h-[460px] border-2 bg-slate-100 rounded-xl shadow-lg'>
+                    <div className='h-[260px]'>
+                        <img className='h-[100%] w-[100%] rounded-xl' style={{objectFit:'cover', objectPosition:'center'}} src={Outreach} alt="card-image" />
                     </div>
                     <div>
-                        <h1>Training</h1>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                        <h1 className='text-center text-2xl font-medium'>Outreach</h1>
+                        <p className='px-[20px] text-center'>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
                             Corrupti labore voluptatem voluptatum adipisci tenetur
                             placeat veniam iusto ducimus repellat blanditiis eveniet, 
                             mollitia error voluptatibus aspernatur delectus ea qui nihil 
