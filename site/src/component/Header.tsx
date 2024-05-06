046f100f7dff0e4a78a35339fb0898d9375c324d
emperorbj <cwaku96@gmail.com>
2024-05-06 12:12:17 -0700

    added background image with glass effect on the navigation bar

--- site/src/component/Header.tsx
+++ site/src/component/Header.tsx
@@ -1,10 +1,15 @@
-import React from 'react'
+import HeaderPix from '../assets/header1.jpg';
 import Navbar from './Navbar'
 
 const Header = () => {
     return (
-        <div className='h-[30%]'>
+        <div className='h-[550px] w-full'>
             <Navbar/>
+            <img className='mt-[-80px]' src={HeaderPix} style={{
+            height:'100%',
+            width:'100%',
+            objectPosition:'center',
+            objectFit:'cover'}} alt="header background" />
         </div>
     )
 }
--- site/src/component/Navbar.tsx
+++ site/src/component/Navbar.tsx
@@ -5,21 +5,21 @@ import { DropdownTwo } from './DropdownTwo';
 
 const Navbar = () => {
     return (
-        <nav className='flex items-center justify-between w-full h-[80px] bg-white/20 backdrop-blur-lg shadow-lg z-30'>
+        <nav className='flex items-center justify-between w-full h-[80px] bg-white/20 backdrop-blur-sm shadow-lg z-30'>
             <Link to='/'>
                 <div className='w-[100px]  px-[10px]  mx-10 text-3xl'>
                     logo
                 </div>
             </Link>
             <div className='flex gap-[100px] mr-[60px]'>
-                <Link to="/" className='hover:bg-accent w-[110px] text-center text-lg font-bold rounded-lg py-[12px]  px-[10px] '>
+                <Link to="/" className='hover:border hover:border-input hover:bg-white/20 w-[110px] text-center text-lg font-bold rounded-lg py-[12px]  px-[10px] '>
                     < button>Home</ button>
                 </Link>
-                <Link to="/about" className='hover:bg-accent w-[110px] text-center text-lg font-bold rounded-lg py-[12px]  px-[10px] '>
+                <Link to="/about" className='hover:border hover:border-input hover:bg-white/20 w-[110px] text-center text-lg font-bold rounded-lg py-[12px]  px-[10px] '>
                     < button>About us</ button>
                 </Link>
-                <Link to="/contact" className='hover:bg-accent w-[110px] text-center text-lg font-bold rounded-lg py-[12px]  px-[10px] '>
-                    < button>Contact us</ button>
+                <Link to="/contact" className='hover:border hover:border-input hover:bg-white/20 w-[110px] text-center text-lg font-bold rounded-lg py-[12px]  px-[10px] '>
+                    < button>Contact</ button>
                 </Link>
                 <Link to="/resources">
                     <DropdownTwo/>
