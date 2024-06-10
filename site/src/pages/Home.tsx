c8f4c009541d139996b77ef3d690e171e4690ab2
emperorbj <cwaku96@gmail.com>
2024-06-10 01:42:09 +0100

    tested site with chatvot

--- site/src/pages/Home.tsx
+++ site/src/pages/Home.tsx
@@ -1,23 +1,14 @@
 import Header from '@/component/Header';
 import Cards from '@/component/Cards';
-import Banner from '@/assets/banner1.1.jpg';
+import Banner from '@/assets/download.jpeg';
 const Home = () => {
     return (
         <div className='w-full h-screen'>
             <Header/>
             <Cards/>
-            <section className='h-[500px] w-full mt-[-15px] relative'>
-                <img className='h-[100%] w-[100%] rounded-xl' style={{objectFit:'cover', objectPosition:'center', opacity:'0.7'}} src={Banner} alt="banner1" />
-                <h1 className='absolute top-[180px] left-[100px] right-[100px] text-5xl font-bold text-white text-center'>Committed to building men to fulfill God's plan for them</h1>
-            </section>
-            <section className='h-[500px] w-full mt-[-15px] relative'>
-                <img className='h-[100%] w-[100%] rounded-xl' style={{objectFit:'cover', objectPosition:'center', opacity:'0.7'}} src={Banner} alt="banner1" />
-                <h1 className='absolute top-[180px] left-[100px] right-[100px] text-5xl font-bold text-white text-center'>Committed to building men to fulfill God's plan for them</h1>
-            </section>
-            <section className='h-[500px] w-full mt-[-15px] relative'>
-                <img className='h-[100%] w-[100%] rounded-xl' style={{objectFit:'cover', objectPosition:'center', opacity:'0.7'}} src={Banner} alt="banner1" />
-                <h1 className='absolute top-[180px] left-[100px] right-[100px] text-5xl font-bold text-white text-center'>Committed to building men to fulfill God's plan for them</h1>
-            </section>
+            {/* <section className='h-[500px] w-full mt-[4px]'>
+                <img className='h-[100%] w-[100%] object-cover' src={Banner} alt="banner1" />
+            </section> */}
         </div>
     )
 }
