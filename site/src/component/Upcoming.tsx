0eefa89e30cd8712fb5253da241e6feaf6ab2b16
emperorbj <cwaku96@gmail.com>
2024-06-10 02:41:34 +0100

    added upcoming component

---
+++ site/src/component/Upcoming.tsx
@@ -0,0 +1,16 @@
+
+
+const Upcoming = () => {
+    return (
+        <div>
+            <div>
+                <img src="" alt="" />
+            </div>
+            <div>
+                
+            </div>
+        </div>
+    )
+}
+
+export default Upcoming
--- site/src/pages/Home.tsx
+++ site/src/pages/Home.tsx
@@ -1,14 +1,12 @@
 import Header from '@/component/Header';
 import Cards from '@/component/Cards';
-import Banner from '@/assets/download.jpeg';
+import Upcoming from '@/component/Upcoming';
 const Home = () => {
     return (
         <div className='w-full h-screen'>
             <Header/>
             <Cards/>
-            {/* <section className='h-[500px] w-full mt-[4px]'>
-                <img className='h-[100%] w-[100%] object-cover' src={Banner} alt="banner1" />
-            </section> */}
+            <Upcoming/>
         </div>
     )
 }
