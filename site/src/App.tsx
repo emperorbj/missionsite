1f3bb833a5581a545058b3feeda8e4998047b647
emperorbj <cwaku96@gmail.com>
2024-08-22 09:08:29 +0100

    "Replace useEffect with useLayoutEffect and uncomment script injection code"

--- site/src/App.tsx
+++ site/src/App.tsx
@@ -3,7 +3,8 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css'
 import Home from './pages/Home'
 import Resources from "./pages/Resources";
-import { useEffect } from 'react';
+// import { useEffect } from 'react';
+import { useLayoutEffect } from 'react';
 import Contact from './pages/Contact';
 import About from './pages/About';
 import Blog from './pages/Blog';
@@ -14,7 +15,17 @@ import Reason from './pages/Reason';
 
 function App() {
 
-  useEffect(() => {
+  // useEffect(() => {
+  //   const script = document.createElement('script');
+  //   script.innerHTML = `
+  //   window.codySettings = { widget_id: '9c3dc3c3-e9af-4f23-9090-f93e4cc312e2' };
+
+  //   !function(){var t=window,e=document,a=function(){var t=e.createElement("script");t.type="text/javascript",t.async=!0,t.src="https://trinketsofcody.com/cody-widget.js";var a=e.getElementsByTagName("script")[0];a.parentNode.insertBefore(t,a)};"complete"===document.readyState?a():t.attachEvent?t.attachEvent("onload",a):t.addEventListener("load",a,!1)}();
+  //   `;
+  //   document.body.appendChild(script);
+  // }, []);
+
+  useLayoutEffect(() => {
     const script = document.createElement('script');
     script.innerHTML = `
     window.codySettings = { widget_id: '9c3dc3c3-e9af-4f23-9090-f93e4cc312e2' };
