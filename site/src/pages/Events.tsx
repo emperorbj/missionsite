afb222ded2ae6881ec2cf7bb593048690045439b
emperorbj <cwaku96@gmail.com>
2024-08-22 10:30:32 +0100

    "Updated Events.tsx: replaced video with img, removed Lottie and bookUs imports, and updated grid items with new images and styles."

--- site/src/pages/Events.tsx
+++ site/src/pages/Events.tsx
@@ -1,7 +1,8 @@
 import Footer from "@/component/Footer"
 import Navbar from "@/component/Navbar"
-import Lottie from 'lottie-react'
-import bookUs from '../Chatting.json'
+import Lottie from 'lottie-react';
+import bookUs from '../Chatting.json';
+import ReachOut from '../assets/reachOut.webp'
 
 const Events = () => {
     return (
@@ -12,7 +13,7 @@ const Events = () => {
             font-myWorkSans py-1 md:py-[10px] 
             rounded-[10px] h-[900px] md:h-1100px lg:h-[650px] bg-slate-200 mx-1 md:mx-5 lg:mx-[40px]">
                 <div className="md:ml-[20px] h-[50%] w-[100%]  md:h-[60%] md:w-[95%] lg:w-[50%] lg:h-[100%] ">
-                    <video className="rounded-[30px] shadow h-[100%] w-[100%] object-cover" src="" loop autoPlay muted />
+                    <img className="rounded-[30px] shadow h-[100%] w-[100%] object-cover" src={ReachOut} alt="" />
                 </div>
                 <div className="h-[50%] w-[95%] md:h-[40%] md:w-[95%] lg:w-[50%] lg:h-[100%] flex flex-col gap-5 
                 px-[30px] items-center justify-center">
@@ -31,9 +32,9 @@ const Events = () => {
                     <h1 className='text-center font-myPoppins text-3xl font-bold mt-40 mb-[20px]'>FOCUS GROUPS</h1>
                     <div className="grid bg-slate-200 rounded-[30px] grid-cols-1 
                     md:grid-cols-2 lg:grid-cols-3 gap-2 md:gap-8 p-4 lg:p-10 md:mx-2 lg:mx-[20px] mb-[20px]">
-                        <div className="md:w-[360px] lg:w-[450px] h-[300px] bg-purple-700 rounded-[8px] flex flex-col gap-3">
-                            <div className="h-[360px] shadow-md rounded-[8px] bg-slate-600">
-                                <img alt="" className='object-cover' src=""/>
+                        <div className="md:w-[360px] lg:w-[450px] h-[360px]rounded-[8px] flex flex-col gap-3">
+                            <div className="h-[320px] shadow-md rounded-[8px]">
+                                <img alt="" className='h-full w-full object-cover' src="https://www.msureporter.com/wp-content/uploads/2019/09/evangelism.jpg"/>
                             </div>
                             <div className="w-[100%] h-[100px] flex flex-col gap-5 items-center justify-center">
                                 <h1 className='font-semibold text-xl'>
@@ -42,9 +43,9 @@ const Events = () => {
                                 
                             </div>
                         </div>
-                        <div className="md:w-[360px] lg:w-[450px] bg-purple-700 h-[300px]  rounded-[8px] flex flex-col gap-3">
-                            <div className="h-[360px] shadow-md rounded-[8px] bg-slate-600">
-                                <img alt="" className='object-cover' src="" />
+                        <div className="md:w-[360px] lg:w-[450px] h-[360px]  rounded-[8px] flex flex-col gap-3">
+                            <div className="h-[320px] shadow-md rounded-[8px]">
+                                <img alt="" className='object-cover h-full w-full' src="https://globalconference.ca/wp-content/uploads/2023/07/What-Is-a-Church-Conference.png" />
                             </div>
                             <div className="w-[100%] h-[100px] flex flex-col gap-5 items-center justify-center">
                                 <h1 className='font-semibold text-xl'>
@@ -52,9 +53,9 @@ const Events = () => {
                                 </h1>
                             </div>
                         </div>
-                        <div className="md:w-[360px] lg:w-[450px] h-[300px] bg-purple-700 rounded-[8px] flex flex-col gap-3">
-                            <div className="h-[360px] shadow-md rounded-[8px] bg-slate-600">
-                                <img alt="" className='object-cover' src=""/>
+                        <div className="md:w-[360px] lg:w-[450px] h-[360px] rounded-[8px] flex flex-col gap-3">
+                            <div className="h-[320px] shadow-md rounded-[8px]">
+                                <img alt="" className='h-full w-full object-cover' src="https://gocic.org/wp-content/uploads/2019/12/sr_437818-1.jpg"/>
                             </div>
                             <div className="w-[100%] h-[100px] flex flex-col gap-5 items-center justify-center">
                                 <h1 className='font-semibold text-xl'>
@@ -62,27 +63,29 @@ const Events = () => {
                                 </h1>
                             </div>
                         </div>
-                        <div className="md:w-[360px] lg:w-[450px] h-[300px] bg-purple-700 rounded-[8px] flex flex-col gap-3">
-                            <div className="h-[360px] shadow-md rounded-[8px] bg-slate-600">
-                                <img alt="" className='object-cover' src=""/>
+                        <div className="md:w-[360px] lg:w-[450px] h-[360px] rounded-[8px] flex 
+                        flex-col gap-3">
+                            <div className="h-[320px] shadow-md rounded-[8px]">
+                            <img alt="" className='h-full w-full object-cover' src="https://thecophq.org/wp-content/uploads/2022/12/Youth-Ministry-Launches-Nationwide-Teens-Evangelism-Drive-2000x1125.jpg"/>
                             </div>
-                            <div className="w-[100%] h-[100px] flex flex-col gap-5 items-center justify-center">
+                            <div className="w-[100%] h-[100px] flex gap-5 items-center justify-center">
                                 <h1 className='font-semibold text-xl'>
                                     High School Outreach
                                 </h1>
                             </div>
                         </div>
-                        <div className="md:w-[360px] lg:w-[450px] h-[300px] bg-purple-700 rounded-[8px] flex flex-col gap-3">
-                            <div className="h-[360px] shadow-md rounded-[8px] bg-slate-600">
-                                <img alt="" className='object-cover' src=""/>
+                        <div className="md:w-[360px] lg:w-[450px] h-[360px] rounded-[8px] 
+                        flex flex-col gap-3">
+                            <div className="h-[320px] shadow-md rounded-[8px]">
+                                <img alt="" className='h-full w-full object-cover' src="https://www.pushingjesusagenda.org/images/jux_gallery/str0004.jpg"/>
                             </div>
-                            <div className="w-[100%] h-[100px] flex flex-col gap-5 items-center justify-center">
+                            <div className="w-[100%] h-[100px] flex  gap-5 items-center justify-center">
                                 <h1 className='font-semibold text-xl'>Street Evangelism</h1>
                             </div>
                         </div>
-                        <div className="md:w-[360px] lg:w-[450px] h-[300px] bg-purple-700 rounded-[8px] flex flex-col gap-3">
-                            <div className="h-[360px] shadow-md rounded-[8px] bg-slate-600">
-                                <img alt="" className='object-cover' src=""/>
+                        <div className="md:w-[360px] lg:w-[450px] h-[360px] rounded-[8px] flex flex-col gap-3">
+                            <div className="h-[320px] shadow-md rounded-[8px]">
+                                <img alt="" className='w-full h-full object-cover' src="https://www.churchrelevance.com/wp-content/uploads/2019/09/Church-Conference.jpg"/>
                             </div>
                             <div className="w-[100%] h-[100px] flex flex-col gap-5 items-center justify-center">
                                 <h1 className='font-semibold text-xl'>Missionary Summits</h1>
