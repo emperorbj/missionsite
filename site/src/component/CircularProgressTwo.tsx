6f3606941bf3dae107afc5cdd59ec68265ffc4d1
emperorbj <cwaku96@gmail.com>
2024-12-29 10:34:34 -0800

    new changes

--- site/src/component/CircularProgressTwo.tsx
+++ site/src/component/CircularProgressTwo.tsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from "react";
-
 import AnimatedCircularProgressBar from "@/components/ui/animated-circular-progress-bar";
 
 const CircularProgressTwo = () => {
@@ -7,25 +6,28 @@ const CircularProgressTwo = () => {
 
     useEffect(() => {
         const handleIncrement = (prev: number) => {
-            if (prev === 100) {
+            if (prev === 10) {
                 return 0;
             }
-            return prev + 10;
+            return prev + 2;
         };
-        setValue(handleIncrement);
-        const interval = setInterval(() => setValue(handleIncrement), 2000);
+
+        const interval = setInterval(() => {
+            setValue((prev) => handleIncrement(prev)); // Use functional updater
+        }, 1000);
+
         return () => clearInterval(interval);
     }, []);
 
     return (
         <AnimatedCircularProgressBar
-            max={100}
+            max={10}
             min={0}
             value={value}
             gaugePrimaryColor="rgb(79 70 229)"
             gaugeSecondaryColor="rgba(0, 0, 0, 0.1)"
         />
-    )
-}
+    );
+};
 
-export default CircularProgressTwo
+export default CircularProgressTwo;
